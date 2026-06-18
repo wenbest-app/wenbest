dde1a29517b3c92f9a947fa0a2132150a37dd7bb
wenbest-app <wenbest.app@gmail.com>
2026-06-18 17:45:32 +0400

    Add Google Analytics to layout

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -2,6 +2,7 @@ import { Stack } from 'expo-router';
 import * as SplashScreen from 'expo-splash-screen';
 import { StatusBar } from 'expo-status-bar';
 import { useEffect } from 'react';
+import { Platform } from 'react-native';
 
 SplashScreen.preventAutoHideAsync();
 
@@ -9,7 +10,21 @@ export default function RootLayout() {
   useEffect(() => {
     const prepareApp = async () => {
       try {
-        // لاحقاً نضيف تحميل الخطوط أو الإعدادات هنا
+        if (Platform.OS === 'web') {
+          const script1 = document.createElement('script');
+          script1.async = true;
+          script1.src = 'https://www.googletagmanager.com/gtag/js?id=G-L91K64CYM0';
+          document.head.appendChild(script1);
+
+          const script2 = document.createElement('script');
+          script2.innerHTML = `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-L91K64CYM0');
+          `;
+          document.head.appendChild(script2);
+        }
       } catch (error) {
         console.warn(error);
       } finally {
